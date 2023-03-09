3b70c500689b3a24ec1c3cf58b9f2f0fb798530d
Kseniya <nuncivolt@gmail.com>
2023-03-09 18:35:26 +0300

    Bug fixes

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,22 @@
 import PostModel from "../models/Post.js";
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map(obj => obj.tags)
+      .flat()
+      .slice(0, 5);
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed!",
+    });
+  }
+};
+
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate("user").exec();
@@ -40,7 +57,7 @@ export const getOne = async (req, res) => {
         }
         res.json(doc);
       }
-    );
+    ).populate('user');
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -90,7 +107,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(","),
       user: req.userId,
     });
 
@@ -117,7 +134,7 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userId,
-        tags: req.body.tags,
+        tags: req.body.tags.split(","),
       }
     );
     res.json({
